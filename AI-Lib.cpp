ec75b2b885ad7a1666cccade98f7fd15b19273b2
ytmak <ytmak@DESKTOP-1UEGI6C>
2024-09-23 09:05:10 +0300

    Add project files.

---
+++ AI-Lib.cpp
@@ -0,0 +1,13 @@
+// AI-Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+
+// TODO: This is an example of a library function
+void fnAILib()
+{
+	std::cout << "Hi" << std::endl;
+}
+		
